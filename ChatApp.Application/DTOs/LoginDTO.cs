2b90bdc76e493002042f382e16ea5c0384989772
ziad abdo <ziad.abdo.dev@gmail.com>
2026-06-10 18:34:04 +0300

    Improve API validation and response models

--- ChatApp.Application/DTOs/LoginDTO.cs
+++ ChatApp.Application/DTOs/LoginDTO.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ChatApp.Application.DTOs;
 
 public class LoginDTO
 {
+	[Required]
 	public string Email { get; set; } = string.Empty;
+
+	[Required]
 	public string Password { get; set; } = string.Empty;
 }
---
+++ ChatApp.Application/DTOs/MessageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.Application.DTOs
+{
+	public class MessageDTO
+	{
+		public string SenderName { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+		public DateTime SentAt { get; set; }
+	}
+}
